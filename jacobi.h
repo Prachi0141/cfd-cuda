fff19b474ec5add271477642a189e3414c26d02b
Prachi0141 <prrachiagarwal2230473@gmail.com>
2020-12-17 17:43:53 +0530

    minor

--- jacobi.h
+++ jacobi.h
@@ -5,3 +5,5 @@ void jacobistep(double *psinew, double *psi, int m, int n);
 double deltasq(double *newarr, double *oldarr, int m, int n);
 
 void jacobiiter_gpu(double *psi, int m, int n, int numiter, double &error);
+
+__global__ void jacobikernel(double *psi_d, double *psinew_d, int m, int n, int numiter);
